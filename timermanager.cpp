815815703feca1a756e16174cfe5cd7b31b1b05c
freemanoid <freemanoid321@gmail.com>
2012-05-09 18:36:10 +0300

    Several premerge stability fixes -Improve antilag check -Small fixes

--- timermanager.cpp
+++ timermanager.cpp
@@ -1,23 +1,32 @@
 #include "timermanager.h"
 
 TimerManager::TimerManager(QObject *parent) :
-    QObject(parent),
-    m_currentTimer(this)
+    QObject(parent)
 {
     m_currentTimer.setSingleShot(true);
     connect(&m_currentTimer, SIGNAL(timeout()), this, SLOT(sendEventOccuredSignal()));
 }
 
-void TimerManager::updateTimer(QList<events> *List)
+void TimerManager::updateTimer(QList<events> List)
 {
     m_currentEventList = List;
+//    for(int i = 0; i < List.size(); i++)
+//        qDebug() << List.at(i).id << " " << List.at(i).cabinet << " " << List.at(i).time << " " << List.at(i).date << " " << List.at(i).inf;
     //it is not important which event is taken because they all have the same time of occured
-    qint64 timerTime = QDateTime::currentDateTime().msecsTo(QDateTime(List->at(0).date, List->at(0).time));
-    if(timerTime < 0 && (-1 * timerTime) < AntiLagTimeMsec)
+    qint64 timerTime = QDateTime::currentDateTime().msecsTo(QDateTime(List.at(0).date, List.at(0).time));
+    if(timerTime < 0)
     {
-        sendEventOccuredSignal();
-        qDebug() << "\"lag\" event occured";
-        return;
+        if((-1 * timerTime) < AntiLagTimeMsec)
+        {
+            sendEventOccuredSignal();
+            qDebug() << "\"lag\" event occured";
+            return;
+        }
+        else
+        {
+            qDebug() << "timer time cannot be negative";
+            return;
+        }
     }
     m_currentTimer.start(timerTime);
     qDebug() << "timer started in " << timerTime << " msecs";
